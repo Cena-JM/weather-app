dfccc2e0db46b69b66c138e8b952b7e272e9bdc0
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-07-22 12:59:59 +0300

    Add toggle styles

--- src/render_form.js
+++ src/render_form.js
@@ -6,6 +6,9 @@ const weatherForm = () => {
 
     // Create elements
     const head = document.createElement('h1');
+    const toggle = document.createElement('div');
+    const celsiusBtn = document.createElement('button');
+    const fahrenheitBtn = document.createElement('button')
     const inputContainer = document.createElement('form');
     const inputField = document.createElement('input');
     const inputBtn = document.createElement('button');
@@ -13,6 +16,11 @@ const weatherForm = () => {
 
     // Set element attributes
     head.classList.add('title');
+    toggle.classList.add('toggle');
+    celsiusBtn.classList.add('temp-btn', 'current-temp');
+    celsiusBtn.setAttribute('id', 'celsius-btn');
+    fahrenheitBtn.classList.add('temp-btn');
+    fahrenheitBtn.setAttribute('id', 'fahrenheit-btn');
     inputContainer.setAttribute('id', 'form');
     inputContainer.classList.add('input-container');
     inputField.setAttribute('id', 'input-field');
@@ -23,14 +31,19 @@ const weatherForm = () => {
     search.classList.add('fas', 'fa-search');
 
     // Arrange elements
+    toggle.appendChild(celsiusBtn);
+    toggle.appendChild(fahrenheitBtn);
     inputBtn.appendChild(search);
     inputContainer.appendChild(inputField);
     inputContainer.appendChild(inputBtn);
     container.appendChild(head);
+    container.appendChild(toggle);
     container.appendChild(inputContainer);
 
     // Add data to elements
-    head.innerHTML = 'What location are you looking for?'
+    head.innerHTML = 'What location are you looking for?';
+    celsiusBtn.innerHTML = `<sup>o</sup>C`;
+    fahrenheitBtn.innerHTML = `<sup>o</sup>F`;
 
     inputContainer.addEventListener("submit", (e) => {
         e.preventDefault();
